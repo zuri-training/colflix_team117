1d086b24ff6de85645505162e09689798c7f3613
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-08-05 09:58:16 +0100

    make changes to account

--- colflix_team117/accounts/views.py
+++ colflix_team117/accounts/views.py
@@ -5,10 +5,14 @@ from django.contrib import messages
 
 
 #NOTE user authentication not yet completed...password not checked
+
+def check_email_validity(email_account):
+    pass
+
 def register(request):
     if request.method == "POST":
         reg_form = RegisterForm(request.POST)
-
+        
         if reg_form.is_valid():
             name = reg_form.cleaned_data["name"]
             u_name = reg_form.cleaned_data["username"]
@@ -16,6 +20,8 @@ def register(request):
             pswd = reg_form.cleaned_data["password"]
 
             user = User.objects.create_user(name=name, username=u_name, password=pswd, email=mail)
+            print(mail)
+            print(user)
             user.save()
             return redirect("/")
     # if a GET (or any other method) we'll create a blank form and render to the page
