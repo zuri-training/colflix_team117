a06fc44bd6f8e03c28402b52d2b3b5020c4fdd24
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-08-06 07:15:03 +0100

    Converteed form view to class based views

--- colflix_team117/accounts/urls.py
+++ colflix_team117/accounts/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('register', views.Register.as_view(), name='register'),
-    path('login', views.Login.as_view(), name='login'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     path('logout_req', views.logout_req, name='logout_req'),
 
     #path('secret', login_required(TemplateView.as_view(template_name="secret.html")), name="secret"),
--- colflix_team117/accounts/views.py
+++ colflix_team117/accounts/views.py
@@ -2,52 +2,48 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.views import View
 
 
-class Register(View):
-    form_class = RegisterForm
-    template_name = 'register.html'
-    
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, 'register.html', {'form':form})
-    
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        #Note cleaned_data is only available when you use is_valid
-        if form.is_valid():
-            name = form.cleaned_data["name"]
-            u_name = form.cleaned_data["username"]
-            mail = form.cleaned_data["email"]
-            pswd = form.cleaned_data["password"]
+#NOTE user authentication not yet completed...password not checked
+def register(request):
+    if request.method == "POST":
+        reg_form = RegisterForm(request.POST)
 
-            user = User.objects.create_user(first_name=name, username=u_name, password=pswd, email=mail)
+        if reg_form.is_valid():
+            name = reg_form.cleaned_data["name"]
+            u_name = reg_form.cleaned_data["username"]
+            mail = reg_form.cleaned_data["email"]
+            pswd = reg_form.cleaned_data["password"]
+
+            user = User.objects.create_user(name=name, username=u_name, password=pswd, email=mail)
             user.save()
-            return redirect('/')
+            return redirect("/")
+    # if a GET (or any other method) we'll create a blank form and render to the page
+    reg_form = RegisterForm()
+    return render(request, 'register.html', {'reg_form':reg_form})
 
-class Login(View):
-    form_class = LoginForm
-    template_name = 'login.html'
-    
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, 'login.html', {'form':form})
-    
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        
-        if form.is_valid():
-            u_name = form.cleaned_data["username"]
-            pswd = form.cleaned_data["password"]
+
+def login(request):
+    if request.method == 'POST':
+        log_form = LoginForm(request.POST)
+
+        #Note cleaned_data is only available when you use is_valid
+        if log_form.is_valid():
+            u_name = log_form.cleaned_data["username"]
+            pswd = log_form.cleaned_data["password"]
+
             #check if user in database
             user = auth.authenticate(username=u_name, password=pswd)
-        elif user is not None:
+        if user is not None:
             auth.login(request, user)
-            return redirect('/') #redirect to root/home page
+            return redirect('/')
         else:
             messages.info(request, 'wrong username or password')
-            return redirect('login') #back to login page if input is wrong
+            return redirect('login')
+
+    log_form = LoginForm()
+    return render(request, 'login.html', {'log_form':log_form})
+
 
 def logout_req(request):
     auth.logout(request)
