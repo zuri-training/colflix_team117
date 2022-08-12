7705e3a156fc331dab74578342a454e415b26df5
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-08-12 10:41:00 +0100

    collected static

--- colflix_team117/core/models.py
+++ colflix_team117/core/models.py
@@ -1,3 +1,26 @@
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
+class Video(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    file = models.FileField()
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    
+    class Meta:
+        ordering = ('-published',)
+        
+    def __str__(self):
+        return self.title
--- colflix_team117/core/views.py
+++ colflix_team117/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Video
+from .serializers import VideoSerializer
 
 
-class HomePageView(TemplateView): 
-    template_name = 'index.html' # 
-    
 class LandingPageView(TemplateView):
-    template_name = 'landing_page.html' #
+    template_name = 'landing_page.html' 
+    
+class HomePageView(TemplateView): 
+    template_name = 'index.html' 
+    serializer_class = VideoSerializer
+    
