ecfcd3155eeac483cf3c5e8ceff044152f7a0a02
Ridwan Adeyemo <adesolaridwan2003@gmail.com>
2022-08-13 16:28:15 +0100

    fix the database ENGINE

--- colflix_team117/colflix_team117/settings.py
+++ colflix_team117/colflix_team117/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'colflix_team117.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'colflixdb',
         'USER': 'postgres',
         'PASSWORD': '12345',
