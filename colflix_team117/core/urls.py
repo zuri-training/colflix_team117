b15749980326ef27b09f5dd01bc41531d0462b58
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-08-13 08:36:23 +0100

    Configured media

--- colflix_team117/core/urls.py
+++ colflix_team117/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, LandingPageView
+from .views import HomePageView, LandingPageView, PostVideo
 
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
     path('index', HomePageView.as_view(), name='index'),
+    path('post-video', PostVideo.as_view(), name='post-video')
 ]
--- colflix_team117/core/views.py
+++ colflix_team117/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Video
 from .serializers import VideoSerializer
+from rest_framework import generics
 
 
 class LandingPageView(TemplateView):
@@ -11,3 +12,7 @@ class HomePageView(TemplateView):
     template_name = 'index.html' 
     queryset = Video.objects.order_by('-published')
     context_object_name = 'video_list'
+    
+class PostVideo(generics.ListCreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
